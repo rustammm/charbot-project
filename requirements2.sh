a29e315e675868aef510fc9560657fafc76a5d3b
rustammm <rustammm@users.noreply.github.com>
2019-04-23 21:17:07 +0300

    Create requirements2.sh

---
+++ requirements2.sh
@@ -0,0 +1,6 @@
+python3 -m pip install git+https://github.com/Koziev/rutokenizer
+python3 -m pip install git+https://github.com/Koziev/rupostagger
+python3 -m pip install git+https://github.com/Koziev/rulemma
+python3 -m pip install python-crfsuite
+python3 -m pip install git+https://github.com/Koziev/rusyllab
+python3 -m pip install git+https://github.com/Koziev/ruword2tags
