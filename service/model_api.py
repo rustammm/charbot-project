551e1ce9922515fd62f27b417dcd1715dafe2a15
rustammm <rustam960@yandex.ru>
2019-04-23 22:59:38 +0300

    new model

--- service/model_api.py
+++ service/model_api.py
@@ -586,28 +586,6 @@ def rebatch(pad_idx, batch):
     return Batch(src, trg, pad_idx)
 
 
-# In[39]:
-
-
-# GPUs to use
-#devices = [0, 1, 2, 3]
-if False:
-    pad_idx = TGT.vocab.stoi["<blank>"]
-    model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
-    #model.cuda()
-    criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
-    #criterion.cuda()
-    BATCH_SIZE = 12000
-    train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
-                            repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
-                            batch_size_fn=batch_size_fn, train=True)
-    valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
-                            repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
-                            batch_size_fn=batch_size_fn, train=False)
-    model_par = nn.DataParallel(model, device_ids=devices)
-None
-
-
 # In[40]:
 
 
